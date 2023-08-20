062ac22caeaba844fcbe760153e3b1e2093363b3
WageFolabessy <mbappepsg336@gmail.com>
2023-08-20 23:21:45 +0700

    deploy

--- app/Services/impl/LayananPenggunaImpl.php
+++ app/Services/impl/LayananPenggunaImpl.php
@@ -7,6 +7,7 @@ class LayananPenggunaImpl implements LayananPengguna
 {
     private array $users = [
         "richo" => "123",
+        'rinam' => '123'
     ];
 
     function login(string $user, string $password): bool
